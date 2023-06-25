136cab24f29c11f7f8fe16708f3bc27d3993be71
a.artamonov <a.artamonov@gb.ru>
2023-06-25 10:18:58 +0300

    Homework_4 BinaryTree is done

---
+++ src/Homework_4_Tree/Main.java
@@ -0,0 +1,21 @@
+package Homework_4_Tree;
+
+public class Main {
+    public static void main(String[] args) {
+        BinaryTree<Integer> tree = new BinaryTree<>();
+
+        tree.add(5);
+        tree.add(2);
+        tree.add(1);
+        tree.add(8);
+        tree.add(5);
+        tree.add(7);
+        tree.add(3);
+        tree.add(4);
+        tree.add(9);
+        tree.add(0);
+        tree.add(-1);
+
+        tree.print();
+    }
+}
